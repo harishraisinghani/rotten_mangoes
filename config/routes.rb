0de29c755daa310be73f54e66e58512eeff253c6
harishraisinghani <harishraisinghani@gmail.com>
2016-08-16 19:03:27 +0000

    Add links to log in, sign in , log out

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ RottenMangoes::Application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :movies
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   root to: 'movies#index'
 
 end
