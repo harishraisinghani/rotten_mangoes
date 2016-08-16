650c172ed4db725231fa4756bdac3860d701d453
harishraisinghani <harishraisinghani@gmail.com>
2016-08-16 20:22:48 +0000

    Restrict access to create reviews if not current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,12 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def restrict_access
+    if !current_user
+      flash[:alert] = "You must first log in"
+      redirect_to new_session_path
+    end
+  end
+
   helper_method :current_user
 end
