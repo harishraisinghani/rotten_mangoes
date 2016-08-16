d8602bd2990ea359e096889d92724b660086f538
harishraisinghani <harishraisinghani@gmail.com>
2016-08-16 19:31:35 +0000

    Add reviews nested under movies and present only if user logged in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 RottenMangoes::Application.routes.draw do
+  get 'reviews/new'
+
+  get 'reviews/create'
+
+  get 'new/create'
+
   get 'sessions/new'
 
   get 'sessions/create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :movies
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
   root to: 'movies#index'
