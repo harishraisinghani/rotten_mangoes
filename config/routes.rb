f3f677f69877a2e3c5923020c84cc443ec7bc549
harishraisinghani <harishraisinghani@gmail.com>
2016-08-16 18:29:02 +0000

    Add missing helper files and login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 RottenMangoes::Application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :movies
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
   root to: 'movies#index'
 
 end
